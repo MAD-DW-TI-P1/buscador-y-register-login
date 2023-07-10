2c6e542531c5293f4489df90c20dfef8dd145579
Signados <jorgebenitezlopez@gmail.com>
2023-07-10 21:42:09 +0200

    Add 3D

--- src/App.js
+++ src/App.js
@@ -6,37 +6,45 @@ import Login from './Login';
 import Register from './Register';
 import Signup from './Signup';
 import Admin from './Admin';
+import Inmersiva from './Inmersiva';
 
 const Home = () => <h1>Home</h1>
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Front de Cristina Maser
-        </p>
-      </header>
-      <Link to="/">Home</Link>
-      <br/> 
-      <Link to="/invitations">Invitaciones</Link>
-      <br/> 
-      <Link to="/login">Login</Link>
-      <br/> 
-      <Link to="/register">Register</Link>
-      <br/> 
-      <Link to="/signup">Sign up</Link>
-      <br/> 
-      <Link to="/admin">Admin</Link>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/invitations" element={<Invitations />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <div id="controladores">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Front de Cristina Maser
+          </p>
+        </header>
+        <Link to="/">Home</Link>
+        <br/> 
+        <Link to="/invitations">Invitaciones</Link>
+        <br/> 
+        <Link to="/login">Login</Link>
+        <br/> 
+        <Link to="/register">Register</Link>
+        <br/> 
+        <Link to="/signup">Sign up</Link>
+        <br/> 
+        <Link to="/admin">Admin</Link>
+        <br/> 
+        <Link to="/inmersiva">Zona inmersiva</Link>
+      </div>
+      <div id="contenido">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/invitations" element={<Invitations />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/inmersiva" element={<Inmersiva />} />
+        </Routes>
+      </div>
     </div>
   );
 }
